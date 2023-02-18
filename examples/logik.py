b650899707f6b7fe65d325bb16b55586e7343c02
bountycortez <bountycortez@gmail.com>
2023-02-18 18:51:56 +0100

    zwischencommit

---
+++ examples/logik.py
@@ -0,0 +1,33 @@
+# xor von 2 wahren werten
+if True ^ True :
+    print("True ^ True: BEDINGTE ANWEISUNG AUSGEFÜHRT -> WAHR!")
+else:
+    print("True ^ True: BEDINGTE ANWEISUNG NICHT AUSGEFÜHRT -> FALSCH!")
+    
+# xor von 1em wahren und 1em falschen wert
+if True ^ False :
+    print("True ^ False: BEDINGTE ANWEISUNG AUSGEFÜHRT -> WAHR!")
+else:
+    print("True ^ False: BEDINGTE ANWEISUNG NICHT AUSGEFÜHRT -> FALSCH!")
+
+# xor von 1em wahren und 1em falschen wert
+if False ^ True :
+    print("False ^ True: BEDINGTE ANWEISUNG AUSGEFÜHRT -> WAHR!")
+else:
+    print("False ^ True: BEDINGTE ANWEISUNG NICHT AUSGEFÜHRT -> FALSCH!")
+    
+# xor von 2 falschen werten
+if False ^ False :
+    print("False ^ False: BEDINGTE ANWEISUNG AUSGEFÜHRT -> WAHR!")
+else:
+    print("False ^ False: BEDINGTE ANWEISUNG NICHT AUSGEFÜHRT -> FALSCH!")
+    
+# or von 2 wahren werten
+if True or True :
+    print("True or True: BEDINGTE ANWEISUNG AUSGEFÜHRT -> WAHR!")
+else:
+    print("True or True: BEDINGTE ANWEISUNG NICHT AUSGEFÜHRT -> FALSCH!")
+    
+
+    
+    
